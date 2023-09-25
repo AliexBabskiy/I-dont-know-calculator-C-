11de47315a41affb28ac3af3297074069b1ad718
AliexBabskiy <ababskiy@gmail.com>
2023-09-25 18:42:44 +0300

    Добби переделал, Добби свободен

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 
-class Calculator // системное, на этом уровне нечего не ставить
+class Calculator
 {
     static void Main()
     {
-        Console.WriteLine("Добро пожаловать. Нажмите на Enter клавишу для запуска калькулятора.\n"); // \n нужен для переносов строк, ктолько \n столько строк пропустится
-        Console.ReadKey();   // функция ожидания ввода единичного символа
+        Console.WriteLine("Добро пожаловать. Нажмите на Enter клавишу для запуска калькулятора.\n");
+        Console.ReadKey();
 
-        int choice;
-        int num1, num2;
+        object choice;
+        object num1, num2;
         double result;
 
         Console.WriteLine("Список действий:");
@@ -17,58 +17,181 @@ class Calculator // системное, на этом уровне нечего 
         Console.WriteLine("3. Перемножить два числа");
         Console.WriteLine("4. Разделить первое на второе");
         Console.WriteLine("5. Возвести в степень N первое число");
-        Console.WriteLine("6. Найти квадратный корень из числа");        //фн6
-        Console.WriteLine("7. Найти 1 процент от числа");                //фн7
-        Console.WriteLine("8. Найти факториал из числа");                //фн8
-        Console.WriteLine("9. Выйти из программы");                      //фн9 
+        Console.WriteLine("6. Найти квадратный корень из числа");
+        Console.WriteLine("7. Найти 1 процент от числа");
+        Console.WriteLine("8. Найти факториал из числа");
+        Console.WriteLine("9. Выйти из программы");
 
         do
         {
-            Console.WriteLine("Выберите действие:");
-            choice = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:");
+                string singl = Console.ReadLine();
+                if (int.TryParse(singl, out int number))
+                {
+                    choice = number;
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Введены некорректные значения. Попробуйте еще раз. \n");
+                }
+            }
 
             switch (choice)
             {
-                case 1:  // первая функция
-                    Console.WriteLine("Действие: сложение двух чисел");  
-                    Console.Write("Введите первое число: ");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Введите второе число: ");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-                    result = num1 + num2;
+                case 1:  
+                    Console.WriteLine("Действие: сложение двух чисел ");
+                    while (true)
+                    {
+                        Console.Write("Введите первое число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num1 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+
+                    while (true)
+                    {
+                        Console.Write("Введите второе число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num2 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+
+                    result = Convert.ToInt32(num1) + Convert.ToInt32(num2);
                     Console.WriteLine("Сумма равна: " + result);
                     break;
 
-                case 2:     // вторая функция
-                    Console.WriteLine("Действие: вычесть первое число из второго");
-                    Console.Write("Введите первое число: ");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Введите второе число: ");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-                    result = num1 - num2;
+                case 2:     
+                    Console.WriteLine("Действие: вычесть первое число из второго ");
+                    while (true)
+                    {
+                        Console.Write("Введите первое число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num1 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+
+                    while (true)
+                    {
+                        Console.Write("Введите второе число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num2 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+                    result = Convert.ToInt32(num1) - Convert.ToInt32(num2);
                     Console.WriteLine("Результат равен: " + result);
                     break;
 
-                case 3:    // функция 3
-                    Console.WriteLine("Действие: перемножить два числа");
-                    Console.Write("Введите первое число: ");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Введите второе число: ");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-                    result = num1 * num2;
+                case 3:    
+                    Console.WriteLine("Действие: перемножить два числа ");
+                    while (true)
+                    {
+                        Console.Write("Введите первое число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num1 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+
+                    while (true)
+                    {
+                        Console.Write("Введите второе число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num2 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+                    result = Convert.ToInt32(num1) * Convert.ToInt32(num2);
                     Console.WriteLine("Произведение равно: " + result);
                     break;
 
-                case 4:   // функция 4 зачем я это описываю я же и так если что вспомню
-                    Console.WriteLine("Действие: Разделить первое на второе");
-                    Console.Write("Введите первое число: ");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Введите второе число: ");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-
-                    if (num2 != 0)
+                case 4:   
+                    Console.WriteLine("Действие: Разделить первое на второе ");
+                    while (true)
                     {
-                        result = (double)num1 / num2;
+                        Console.Write("Введите первое число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num1 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+                    
+                    while (true)
+                    {
+                        Console.Write("Введите второе число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num2 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+
+                    if (Convert.ToInt32(num2) != 0)
+                    {
+                        result = (double)Convert.ToInt32(num1) / Convert.ToInt32(num2);
                         Console.WriteLine("Результат равен: " + result);
                     }
                     else
@@ -77,53 +200,115 @@ class Calculator // системное, на этом уровне нечего 
                     }
                     break;
 
-                case 5:    // функц.5
-                    Console.WriteLine("Действие: возвести в степень N первое число");
-                    Console.Write("Введите число: ");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Введите степень: ");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-                    result = Math.Pow(num1, num2);
+                case 5:   
+                    Console.WriteLine("Действие: возвести в степень N первое число ");
+                    while (true)
+                    {
+                        Console.Write("Введите первое число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num1 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+
+                    while (true)
+                    {
+                        Console.Write("Введите степень: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num2 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+                    result = Math.Pow(Convert.ToInt32(num1), Convert.ToInt32(num2));
                     Console.WriteLine("Результат равен: " + result);
                     break;
 
-                case 6:   //фн6
-                    Console.WriteLine("Действие: найти квадратный корень из числа");
-                    Console.Write("Введите число: ");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    result = Math.Sqrt(num1);
+                case 6: 
+                    Console.WriteLine("Действие: найти квадратный корень из числа ");
+                    while (true)
+                    {
+                        Console.Write("Введите число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num1 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+                    result = Math.Sqrt(Convert.ToInt32(num1));
                     Console.WriteLine("Квадратный корень: " + result);
                     break;
 
-                case 7:  //фн7
-                    Console.WriteLine("Действие: найти 1 процент от числа");
-                    Console.Write("Введите число: ");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    result = num1 * 0.01;
+                case 7: 
+                    Console.WriteLine("Действие: найти 1 процент от числа ");
+                    while (true)
+                    {
+                        Console.Write("Введите число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num1 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+                    result = Convert.ToInt32(num1) * 0.01;
                     Console.WriteLine("1% от числа равен: " + result);
                     break;
 
-                case 8:  //фн8
-                    Console.WriteLine("Действие: найти факториал из числа");
-                    Console.Write("Введите число: ");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    result = Factorial(num1);
+                case 8: 
+                    Console.WriteLine("Действие: найти факториал из числа ");
+                    while (true)
+                    {
+                        Console.Write("Введите п число: ");
+                        string singl = Console.ReadLine();
+                        if (int.TryParse(singl, out int number))
+                        {
+                            num1 = number;
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода. ");
+                        }
+                    }
+                    result = Factorial(Convert.ToInt32(num1));
                     Console.WriteLine("Факториал равен: " + result);
                     break;
 
-                case 9:   //фн9 несёт в себе только визуальные функции
+                case 9:   
                     Console.WriteLine("Завершение работы.");
-                    break;
-
+                    return;
                 default:
-                    Console.WriteLine("Данная функция отсутствует или введены некорректные значения. Попробуйте еще раз.");
+                    Console.WriteLine("Данная функция отсутствует. Попробуйте еще раз. \n");
                     break;
             }
-            Console.WriteLine();
         }
-        while (choice != 9);
-        
-        Console.ReadKey();  // такая же функция как в начале, только тут она даёт возможность завершить программу с не сразу поэтому видно экран завершение работы
+        while (Convert.ToInt32(choice) != 9);
     }
 
     static double Factorial(int number)
